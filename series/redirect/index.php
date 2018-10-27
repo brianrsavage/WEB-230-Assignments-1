65088fc0174507bbd01b7f072499fe5b5fada1f9
digidoll <36100059+digidoll@users.noreply.github.com>
2018-10-27 02:12:39 -0400

    Add files via upload

---
+++ series/redirect/index.php
@@ -0,0 +1,12 @@
+<h1>My Page</h1>
+
+<?php
+    $redirect_page = 'http://google.com';
+    $redirect = true;
+    if($redirect == true){
+
+      // 01. Explaine what is happening here.
+      // Because of the earlier variables and function, the $redirect is being read as true, and therefore, the page is being redirected to the site declared in $redirect_page.
+      header('Location: '.$redirect_page);
+    }
+ ?>
