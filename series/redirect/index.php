a85643b3df4dee97a1fefa40ef62a58a2e783d0b
Brian Savage <brianrsavage@users.noreply.github.com>
2018-10-31 17:47:27 -0400

    Update index.php

--- series/redirect/index.php
+++ series/redirect/index.php
@@ -5,8 +5,11 @@
     $redirect = true;
     if($redirect == true){
 
-      // 01. Explaine what is happening here.
-      // Because of the earlier variables and function, the $redirect is being read as true, and therefore, the page is being redirected to the site declared in $redirect_page.
+    /*
+        01. Explaine what is happening here.
+        Because of the earlier variables and function, the $redirect is being read as true, 
+        and therefore, the page is being redirected to the site declared in $redirect_page.
+    */
       header('Location: '.$redirect_page);
     }
  ?>
