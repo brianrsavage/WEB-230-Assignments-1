071567eccd6e5be5d520c18fbbfd1a50e099b320
Brian Savage <brianrsavage@users.noreply.github.com>
2018-10-31 17:40:24 -0400

    Update index.php

--- series/get-ip-better/index.php
+++ series/get-ip-better/index.php
@@ -8,9 +8,15 @@
   $remote_addr = $_SERVER['REMOTE_ADDR'];
   echo '<h3>$remote_addr is: '.$remote_addr.'</h3>';
 
-  // 01. Explain what the following conditional statment is doing.
-  // This statement is supposed to be checking to see if the $http_client_ip is not empty -- if it isn't, the $ip_address will be the $http_client_ip. It then checks to see if the $http_x_forwarded_for is not empty -- if it isn't, the $ip_address will be the $http_x_forwarded_for. If either of these are empty, the $ip_address should be $remote_addr, but for whatever reason, it isn't working correctly and I'm just getting errors.
-
+	/* 	
+  	01. Explain what the following conditional statment is doing.
+  	This statement is supposed to be checking to see if the $http_client_ip is not empty 
+	-- if it isn't, the $ip_address will be the $http_client_ip. It then checks to see if 
+	the $http_x_forwarded_for is not empty -- if it isn't, the $ip_address will be 
+	the $http_x_forwarded_for. If either of these are empty, the $ip_address should 
+	be $remote_addr, but for whatever reason, it isn't working correctly and I'm just 
+	getting errors.
+	*/
   if (!empty($http_client_ip)) {
 	$ip_address = $http_client_ip;
   } else if (!empty($http_x_forwarded_for)) {
