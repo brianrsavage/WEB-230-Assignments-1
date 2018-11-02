cae058f882023a2c8bd1c2d2efbca26f078255f7
digidoll <36100059+digidoll@users.noreply.github.com>
2018-11-01 22:32:31 -0400

    Update index.php

--- series/working-with-forms/index.php
+++ series/working-with-forms/index.php
@@ -12,4 +12,27 @@
   <input type="submit" value="Submit">
 </form>
 
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" 
+integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
+    <div class="fluid-container">
+    <div class="row justify-content-center">
+      <div class="col-6">
+        <form action="index.php" method="GET">
+          <div class="form-group" style="padding: 20px 0px 10px; width: 300px;">
+            <label for="day"><h6>Day:</h6></label>
+            <input type="text" name="day" class="form-control form-control-sm" placeholder="Enter day here." value="">
+          </div>
+          <div class="form-group" style="padding: 0px; width: 300px;">
+            <label for="day"><h6>Date:</h6></label>
+            <input type="text" name="date" class="form-control form-control-sm" placeholder="Enter date here." value="">
+          </div>
+          <div class="form-group" style="padding: 10px 0px; width: 300px;">
+            <label for="day"><h6>Year:</h6></label>
+            <input type="number" name="year" class="form-control form-control-sm" placeholder="Enter year here." value="">
+          </div>
+      <div>
+          <div class="col-6" style="margin: 0px auto;">            
+          <input type="submit" value="Submit">
+      </div>
+      </form>
