bf3d01398b94064ef183646fdeddd3d92733b59b
digidoll <36100059+digidoll@users.noreply.github.com>
2018-11-01 21:49:26 -0400

    Add files via upload

---
+++ series/working-with-forms/index.php
@@ -0,0 +1,15 @@
+<!--
+  01. Explain what the action attribute is and does.
+  The action attribute defines /where/ data is sent.
+  02. Explain what the method attribute is and does.
+  The method attribute defines /how/ data is sent.
+-->
+
+<form action="index.php" method="GET">
+  Day:<br/><input type="text" name="day"><br>
+  Date:<br/><input type="text" name="date"><br>
+  Year:<br/><input type="text" name="year"><br><br>
+  <input type="submit" value="Submit">
+</form>
+
+
